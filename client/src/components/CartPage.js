292ba87c7678ea2e20820cc7d34fca0915cf4649
Jason <jason.steelman@sposatogse.org>
2024-02-16 18:15:48 -0500

    notify users when they try to check out without any cart items

--- client/src/components/CartPage.js
+++ client/src/components/CartPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../css/CartPage.css';
 import CartItemCard from './CartItemCard';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const CartPage = () => {
     const cart = useSelector(state => state.cart);
@@ -18,11 +19,18 @@ const CartPage = () => {
                 </form>
             </div>
             <div id='ordered-products-details'>
-                {cart.map((item) => {
-                    return (
-                        <CartItemCard key={item.product_id} product_id={item.product_id} quantity={item.quantity} />
+                {
+                    cart.length > 0 ? cart.map((item) => {
+                        return (
+                            <CartItemCard key={item.product_id} product_id={item.product_id} quantity={item.quantity} />
+                        )
+                    }) : (
+                        <div>
+                            <p>Add some products to your cart to check out!</p>
+                            <Link to="/">Return to products page</Link>
+                        </div>
                     )
-                })}
+                }
             </div>
         </div>
     );
