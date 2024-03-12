985b18191bd678b2f1d50659ace67543f4815386
Jason <jason.steelman@sposatogse.org>
2024-03-12 11:48:22 -0400

    change method for assigning username to google users

--- server/index.js
+++ server/index.js
@@ -107,7 +107,8 @@ passport.use(
         passReqToCallback: true,
     },
     function(request, accessToken, refreshToken, profile, done) {
-        db.findOrCreateGoogleUser({ googleId: profile.id, name: profile.name }, function (err, user) {
+        console.log(profile);
+        db.findOrCreateGoogleUser({ googleId: profile.id, name: profile.displayName }, function (err, user) {
             return done(err, user);
         });
     })
