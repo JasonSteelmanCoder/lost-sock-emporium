82076f6ed8dcccb8567e5c5b3f2aa52b7ae3a5a8
Jason <jason.steelman@sposatogse.org>
2024-03-19 12:46:58 -0400

    look for cookie on req.session

--- server/index.js
+++ server/index.js
@@ -255,7 +255,7 @@ app.get('/auth/google/callback', passport.authenticate('google', {
 app.get('/login', (req, res, next) => {
     // console.log('REQ SESSION: ' + JSON.stringify(req.session));
     // console.log('REQ ISLOGGEDIN: ' + req.isLoggedIn);
-    console.log("COOKIES" + req.cookie);
+    console.log("COOKIES" + req.session.cookie);
     const user_id = req.session.passport && req.session.passport.user ? req.session.passport.user : null; 
     res.json({
         "authenticated": req.isAuthenticated(),
