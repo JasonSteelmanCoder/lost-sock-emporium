90a5838d315d4d598b228937ce7d33c1477193a6
Jason <jason.steelman@sposatogse.org>
2024-03-19 23:41:23 -0400

    stop passing request to callback

--- server/index.js
+++ server/index.js
@@ -105,9 +105,8 @@ passport.use(
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
         callbackURL: "https://lost-sock-emporium-backend.onrender.com/auth/google/callback",
-        passReqToCallback: true,
     },
-    async function(request, accessToken, refreshToken, profile, cb) {
+    async function(accessToken, refreshToken, profile, cb) {
         await db.findOrCreateGoogleUser({ googleId: profile.id, name: profile.email }, 
         function (err, user) {
             if (err) {
