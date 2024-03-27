a283e2550303a43848874e5a51b3d8d0856df13c
Jason <jason.steelman@sposatogse.org>
2024-03-27 13:49:38 -0400

    add loading page

---
+++ client/src/components/LoadingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/LoadingPage.css';
+
+const LoadingPage = () => {
+    
+    return (
+        <div id="LoadingPage">
+            <h1>Loading...<br/><br/>Please wait.</h1>
+        </div>
+    )
+}
+
+export default LoadingPage;
