e4da8b9eecb82082e2bdbafb54861c9741d78a1c
Jason <jason.steelman@sposatogse.org>
2024-03-30 09:57:24 -0400

    order history page directs you to login if necessary

--- client/src/components/OrderHistory.js
+++ client/src/components/OrderHistory.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../css/OrderHistory.css';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const OrderHistory = () => {
-    
+    const authenticated = useSelector((state) => state.auth.authenticated);
+
     return (
         <div id='order-history'>
-            <p>Your order history...</p>
+            {
+                !authenticated 
+                ? <p>Please <Link to="/login">login</Link> to see your order history.</p> 
+                : <p>Order history...</p>
+            }
         </div>
     )
 }
