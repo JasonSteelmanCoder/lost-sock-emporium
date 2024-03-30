72e5c7233355f9df622e0c1c43a5490ef11eaa03
Jason <jason.steelman@sposatogse.org>
2024-03-30 07:56:57 -0400

    add order history page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import RegistrationPage from './components/RegistrationPage.js';
 import ProductPage from './components/ProductPage.js';
 import LoadingPage from './components/LoadingPage.js';
 import { createRoutesFromElements, RouterProvider, Route, createBrowserRouter } from 'react-router-dom';
+import OrderHistory from './components/OrderHistory.js';
 
 const appRouter = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Root />}>
@@ -16,6 +17,7 @@ const appRouter = createBrowserRouter(createRoutesFromElements(
     <Route path='/register' element={<RegistrationPage />} />
     <Route path='/products/:product_id' element={<ProductPage />} />
     <Route path='/loading' element={<LoadingPage />} />
+    <Route path='/history' element={<OrderHistory />} />
   </Route>
 ))
 
---
+++ client/src/components/OrderHistory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/OrderHistory.css';
+
+const OrderHistory = () => {
+    
+    return (
+        <div id='order-history'>
+            <p>Your order history...</p>
+        </div>
+    )
+}
+
+export default OrderHistory;
