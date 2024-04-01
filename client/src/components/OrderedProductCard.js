7ff041277e086475e349f948fdc4547a28496f5a
Jason <jason.steelman@sposatogse.org>
2024-04-01 14:40:51 -0400

    orderedProductCards load images of their products

--- client/src/components/OrderedProductCard.js
+++ client/src/components/OrderedProductCard.js
@@ -1,29 +1,41 @@
 import React, { useState, useEffect } from "react";
 import { fetchProductById } from "../API_helpers/APIHelpers";
+import { API_ENDPOINT } from "../API_helpers/APIEndpoint";
 
 const OrderedProductCard = ({orderedProductId, orderedProductQuantity}) => {
 
     const [product, setProduct] = useState({});
+    const [image, setImage] = useState([]);
 
     useEffect(() => {
         const getProduct = async () => {
             const response = await fetchProductById(orderedProductId);
-            console.log(response);
             setProduct(response);
         }
 
         getProduct();
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        const getImage = async () => {
+            const data = await fetch(`${API_ENDPOINT}/images/${product.image_name}`);
+            const blob = await data.blob();
+            const url = URL.createObjectURL(blob);
+            setImage(url);
+        }
+
+        getImage();
+    }, [product])
 
     return (
         <div id="ordered-product-card">
-            <div>{/* product image */}</div> 
+            <img src={image} /> 
             <div>
                 <h3>Product: {product.product_name}</h3>
                 <p>Description: {product.description}</p>
                 <span>Quantity: {orderedProductQuantity}</span>
-                <span>Price: {product.price}</span>
-                <span>Subtotal: ${Number(product.price.slice(1)) * orderedProductQuantity}</span>
+                <span>Price: {product.price ? product.price : " "}</span>
+                <span>Subtotal: ${product.price ? Number(product.price.slice(1)) * orderedProductQuantity : " "}</span>
             </div>
         </div>
     )
