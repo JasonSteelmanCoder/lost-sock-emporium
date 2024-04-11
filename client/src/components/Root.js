9de88a5e0680e3e1ca04dbbbcd23ae6a02f2c4f5
Jason <jason.steelman@sposatogse.org>
2024-04-11 09:34:29 -0400

    root links are disabled on loading page

--- client/src/components/Root.js
+++ client/src/components/Root.js
@@ -8,10 +8,13 @@ import { logout } from '../API_helpers/APIHelpers';
 import store from '../store.js';
 import { signalLoggedOut } from './authSlice';
 import { emptyCart } from './cartSlice.js';
+import { useLocation } from 'react-router-dom';
 
 const Root = () => {
     const loggedIn = useSelector(store => store.auth.authenticated);
     const cart = useSelector(store => store.cart)
+    const location = useLocation();
+    let isLoading = location.pathname === '/loading';
 
     const handleLogOut = async (event) => {
         await logout();
@@ -24,17 +27,17 @@ const Root = () => {
         <div>
             <nav>
                 <div id='logo-container'>
-                    <Link to={'/'} >
+                    <Link to={'/'} className={isLoading ? 'disabled' : null} >
                         <img src={logoImage} id='logo' alt='sock emporium logo' ></img>
                     </Link>
                 </div>
-                <Link to={'history'} id='root-history-button' >
+                <Link to={'history'} id='root-history-button' className={isLoading ? 'disabled' : null}>
                     Orders
                 </Link>
-                <Link to={'cart'} id='root-cart-button' style={{'--cart-length': `"${cart.length}"`, '--show-notification': `${cart.length > 0 ? 1 : -1}`}} >
+                <Link to={'cart'} id='root-cart-button' className={isLoading ? 'disabled' : null} style={{'--cart-length': `"${cart.length}"`, '--show-notification': `${cart.length > 0 ? 1 : -1}`}} >
                     <img src={cartImage} id='cart-image' alt='cart icon' ></img>
                 </Link>
-                {loggedIn ? <button id='root-logout-button' onClick={handleLogOut}>Logout</button> : <Link to={'login'} id='root-login-button'>Login</Link> }
+                {loggedIn ? <button id='root-logout-button' onClick={handleLogOut}>Logout</button> : <Link to={'login'} id='root-login-button' className={isLoading ? 'disabled' : null}>Login</Link> }
             </nav>
             <Outlet />
         </div>
